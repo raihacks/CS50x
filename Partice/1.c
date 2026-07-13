3ccd2bf2d5d1ead106f03e6784e227f7c9c30311
raihacks <siddhirai326@gmail.com>
2026-07-13 21:51:11 +0530

    3

---
+++ Partice/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int marks;
+    char first_name;
+    float pi;
+    scanf("%d %c %f", &marks, &first_name, &pi);
+    printf("a num %d, a char %c, a float value %f", marks, first_name, pi);
+}
